8885851b6aa8eb756d44fa6eb9f50cb568776959
Jueast <jueastwang@outlook.com>
2020-11-27 11:05:31 +0800

    avoid text macro conflicts

--- src/pack_libprotobuf_source.sh
+++ src/pack_libprotobuf_source.sh
@@ -13,18 +13,6 @@ find . -name "*.cc" | xargs -I {} rm {}
 find . -type f | xargs -I {} cp --parents {} ../${MODULE_NAME}/Public
 cd ../${MODULE_NAME}
 
-# ignore warnings 
-echo '
-// Disable some warning
-#ifdef _MSC_VER
-#pragma warning(disable:4065 4125 4800 4946 4582)
-#endif //_MSC_VER
-
-// Disable some warning
-#if defined(_MSC_VER) && defined(_MM_PROTOBUF_LIBRARY_INTERNAL)
-#pragma warning(disable:4127 4100 4276 4389 4018 4267 4457 4310 4506 4146 4996 4661)
-#endif //_MSC_VER
-' >> Public/google/protobuf/port_def.inc
 # replace export tag
 
 find . -type f | grep -v "port" | xargs -I {} sed -i "s/\<PROTOBUF_EXPORT\>/${MODULE_NAME^^}_API/g" {}
